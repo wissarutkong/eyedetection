6638deff82b0e73be5321acbbbcb1fa1d86aa381
wissarutkong <59951365+wissarutkong@users.noreply.github.com>
2020-02-19 14:56:15 +0700

    Add files via upload

---
+++ client_1.py
@@ -0,0 +1,56 @@
+import cv2
+import io
+import socket
+import struct
+import time
+import pickle
+import zlib
+from threading import Thread
+from pygame import mixer
+
+time.sleep(0.1)  
+
+mixer.init()
+sound = mixer.Sound('alarm.wav')
+
+ALARM_ON = False
+last_frame = None
+
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect(('35.240.252.73', 8485))
+connection = client_socket.makefile('wb')
+
+cam = cv2.VideoCapture(0)
+
+def get_frame():
+    global last_frame
+    while True:
+        ret, frame = cam.read()
+        last_frame = frame
+
+
+
+img_counter = 0
+encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 90]
+
+get_frame_thread = Thread(target=get_frame)
+get_frame_thread.start()
+
+cam.set(3, 640);
+cam.set(4, 540);
+while True:
+    if last_frame is not None:
+        frame = last_frame
+        result, frame = cv2.imencode('.jpg', frame, encode_param)
+        data = pickle.dumps(frame, 0)
+        size = len(data)
+        client_socket.sendall(struct.pack(">L", size) + data)
+        datatext = client_socket.recv(4096)
+        
+        datatext = datatext.decode("utf-8").replace(' ','')
+        if(datatext == 'B'):
+            sound.play()
+
+        img_counter += 1
+
+cam.release()
